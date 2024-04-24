dc07bbeb541333a800448b0822a5b5554b593bbe
nchurch250 <158090292+nchurch250@users.noreply.github.com>
2024-04-24 18:12:35 -0500

    edits

--- backend/dataSchema.js
+++ backend/dataSchema.js
@@ -1,7 +1,30 @@
-const mongoose = require('mongoose');
 
-main().catch(err => console.log(err));
 
-async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/reactdata/fakestore_catalog');
-}
+const kittySchema = new mongoose.Schema({
+    name: String
+});
+
+const Kitten = mongoose.model('Kitten', kittySchema);
+
+const silence = new Kitten({ name: 'Silence' });
+console.log(silence.name); // 'Silence'
+
+kittySchema.methods.speak = function speak() {
+    const greeting = this.name
+        ? 'Meow name is ' + this.name
+        : 'I don\'t have a name';
+    console.log(greeting);
+};
+
+
+
+const fluffy = new Kitten({ name: 'fluffy' });
+fluffy.speak(); // "Meow name is fluffy"
+
+await fluffy.save();
+fluffy.speak();
+
+const kittens = await Kitten.find();
+console.log(kittens);
+
+await Kitten.find({ name: /^fluff/ });
--- backend/index.js
+++ backend/index.js
@@ -14,3 +14,42 @@ const host = "localhost";
 app.listen(port, () => {
     console.log("App listening at http://%s:%s", host, port);
 });
+
+const mongoose = require('mongoose');
+
+// main().catch(err => console.log(err));
+
+// async function main() {
+//     await mongoose.connect('mongodb://127.0.0.1:27017/reactdata');
+// }
+
+// Define a schema for your data model
+const productSchema = new mongoose.Schema({
+    name: String,
+    price: Number,
+    category: String
+}, { collection: 'fakestore_catalog' }); // specify the collection name here
+
+// Create a Mongoose model based on the schema
+const Product = mongoose.model('Product', productSchema);
+
+// Connect to MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/reactdata')
+    .then(() => {
+        console.log("MongoDB connected successfully!");
+        // Add a new product
+
+    })
+    .catch(err => {
+        console.error("Error connecting to MongoDB:", err);
+    });
+
+app.post("/product", async (req, res) => {
+    const newProduct = new Product({
+        name: 'Sample Product',
+        price: 10.99,
+        category: 'Electronics'
+    });
+
+    newProduct.save();
+});
